1e22d1956b5373b87ffc4b79fca6d0538e908cbc
Charlotte0327 <charlottegalloway40@gmail.com>
2026-08-06 07:35:11 -0500

    Portfolio, Contact, Resume HTML setup

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+fetch("navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-});
+    const menu = document.querySelector("#mobile-menu");
+    const menuLinks = document.querySelector(".navbar__menu");
+
+    menu.addEventListener("click", function () {
+        menu.classList.toggle("is-active");
+        menuLinks.classList.toggle("active");
+    });
+  });
+
+/*fetch("navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+
+    const menu = document.querySelector("#mobile-menu");
+    const menuLinks = document.querySelector(".navbar__menu");
+
+    console.log(menu);
+    console.log(menuLinks);
+
+    menu.addEventListener("click", function () {
+        console.log("hamburger clicked");
+
+        menu.classList.toggle("is-active");
+        menuLinks.classList.toggle("active");
+    });
+  }); */
